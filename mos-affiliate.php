87b58eedd48fd7a164974f9c810bd9d9cc345486
Martin Cao <martin.g.cao@gmail.com>
2020-10-06 15:33:56 +1100

    Initial commit

---
+++ mos-affiliate.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: MOS Affiliate
+ * Description: Functions, shortcodes and APIs for MOS affiliate system. Currently relies on Indeed Ultimate Affiliate Pro
+ */
+
+ 
---
+++ rclone-config.sh
@@ -0,0 +1,2 @@
+export REMOTE='beta-mos__plugins:mos-affiliate'
+export LOG_FILE='rclone.log'

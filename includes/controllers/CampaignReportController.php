3b7ba8b9110822b40d6c7df776e600cfccb831fa
Martin Cao <martin.g.cao@gmail.com>
2020-10-08 11:33:30 +1100

    Rename campaign_report variable to campaigns and add header variable

--- includes/controllers/CampaignReportController.php
+++ includes/controllers/CampaignReportController.php
@@ -2,6 +2,22 @@
 
 class CampaignReportController extends MosAffiliateController {
 
-  
+  protected $variables = [
+    'campaigns',
+    'headers',
+  ];
+
+
+  protected function campaigns() {
+    $db = MosAffiliateDb::instance();
+    $campaigns = $db->get_campaign_data();
+    return $campaigns;
+  }
+
+
+  protected function headers() {
+    $headers = array_keys((array)$this->campaigns()[0]);
+    return $headers;
+  }
 
 }

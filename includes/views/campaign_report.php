973fadcb177d5d84cc41acaf4d15fa95fab2f121
Martin Cao <martin.g.cao@gmail.com>
2020-10-08 11:31:29 +1100

    Implement basic table for campaign report view

--- includes/views/campaign_report.php
+++ includes/views/campaign_report.php
@@ -1,1 +1,18 @@
-<h1>Campaign Report</h1>
+<table>
+  <thead>
+    <tr>
+      <?php foreach( $headers as $header ):?>
+      <th><?php echo $header; ?></th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($campaigns as $campaign): ?>
+      <tr>
+        <?php foreach( $campaign as $value ): ?>
+        <td><?php echo $value; ?></td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>

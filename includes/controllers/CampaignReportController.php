06d63a502ca69649b8ba0db42ae294093f3208b3
Martin Cao <martin.g.cao@gmail.com>
2020-10-08 18:30:25 +1100

    Implement partners column in campaign report

--- includes/controllers/CampaignReportController.php
+++ includes/controllers/CampaignReportController.php
@@ -11,6 +11,22 @@ class CampaignReportController extends MosAffiliateController {
   protected function campaigns() {
     $db = MosAffiliateDb::instance();
     $campaigns = $db->get_campaign_data();
+
+    // Add empty partners column to campaigns
+    foreach ( $campaigns as &$campaign ) {
+      $campaign['partners'] = 0;
+    }
+    
+    // Get a list of referrals
+    $referrals = $db->get_referrals(['level', 'campaign']);
+
+    // Count partners
+    foreach ( $referrals as $referral ) {
+      if ( strpos( 'partner', $referral->level) !== false ) {
+        $campaigns[$referral->campaign]['partners']++;
+      }
+    }
+
     return $campaigns;
   }
 

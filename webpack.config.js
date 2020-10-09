9d418d4c34f1b7bf00d1a8c116d79addaf8726d8
Martin Cao <martin.g.cao@gmail.com>
2020-10-09 15:45:55 +1100

    Rename bundle js file to mosAffiliate.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    filename: 'mosAffiliate.js'
   },
   watch: true,
   watchOptions: {

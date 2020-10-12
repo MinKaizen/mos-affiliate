168cbdeb7273fda5f3907d62f0a76042ed359d89
Martin Cao <martin.g.cao@gmail.com>
2020-10-12 12:07:58 +1100

    Implement export csv button

--- src/js/components/CampaignReport.js
+++ src/js/components/CampaignReport.js
@@ -1,10 +1,20 @@
 const $ = require('jquery');
-require('datatables.net');
+require( 'datatables.net-dt' );
+require( 'datatables.net-buttons-dt' );
+require( 'datatables.net-buttons/js/buttons.html5.js' );
 
 export default class campaignReport {
 
   constructor(element) {
-    let options = []
+    let options = {
+      dom: 'Bfrtip',
+      buttons: [
+        {
+            extend: 'csv',
+            text: 'Download CSV'
+        },
+      ]
+    }
     $(element).DataTable(options)
   }
 

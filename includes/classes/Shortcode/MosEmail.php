4021e125fddb489110ff75fff4ee1e82ebba4787
Martin Cao <martin.g.cao@gmail.com>
2020-11-02 20:17:37 +1100

    Add shortcode mos_email

---
+++ includes/classes/Shortcode/MosEmail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MOS\Affiliate\Shortcode;
+
+use MOS\Affiliate\Shortcode;
+use MOS\Affiliate\User;
+
+class MosEmail extends Shortcode {
+
+  protected $slug = 'mos_email';
+
+  public function shortcode_action( $args ): string {
+    $user = User::current();
+    $email = $user->user_email;    
+    return $email;
+  }
+
+}

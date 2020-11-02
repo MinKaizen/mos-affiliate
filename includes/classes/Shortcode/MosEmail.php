3cf505a044bf88eaaeb44d70a86b567d121205dc
Martin Cao <martin.g.cao@gmail.com>
2020-11-03 09:21:43 +1100

    Use email() function instead of user_email property

--- includes/classes/Shortcode/MosEmail.php
+++ includes/classes/Shortcode/MosEmail.php
@@ -11,7 +11,7 @@ class MosEmail extends Shortcode {
 
   public function shortcode_action( $args ): string {
     $user = User::current();
-    $email = $user->user_email;    
+    $email = $user->email();    
     return $email;
   }
 

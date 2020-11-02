5ce5e9c95c765b4ee2a3c295304080e785eb5e51
Martin Cao <martin.g.cao@gmail.com>
2020-11-03 09:12:03 +1100

    Add shortcode mos_sponsor_name

---
+++ includes/classes/Shortcode/MosSponsorName.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MOS\Affiliate\Shortcode;
+
+use MOS\Affiliate\Shortcode;
+use MOS\Affiliate\User;
+
+class MosSponsorName extends Shortcode {
+
+  protected $slug = 'mos_sponsor_name';
+
+  public function shortcode_action( $args ): string {
+    $user = User::current();
+    $sponsor = $user->sponsor();
+    $sponsor_name = $sponsor->name();
+    return $sponsor_name;
+  }
+
+}

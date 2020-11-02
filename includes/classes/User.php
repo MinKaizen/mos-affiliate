50e68bbd6cafb501b0f6bf255772773f620dcf00
Martin Cao <martin.g.cao@gmail.com>
2020-11-03 08:53:47 +1100

    Type hint: always return a User object when using alternative constructors

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,19 +5,19 @@ namespace MOS\Affiliate;
 class User extends \WP_User {
 
   
-  public static function current() {
+  public static function current(): self {
     $wpid = \get_current_user_id();
     return self::from_id( $wpid );
   }
 
 
-  public static function from_id( int $wpid ) {
+  public static function from_id( int $wpid ): self {
     $new_user = new self( $wpid );
     return $new_user;
   }
 
 
-  public static function from_affid( int $affid ) {
+  public static function from_affid( int $affid ): self {
     global $wpdb;
 
     $table = $wpdb->prefix . 'uap_affiliates';
@@ -57,6 +57,19 @@ class User extends \WP_User {
     }
 
     return self::from_affid( $sponsor_affid );
+  }
+
+
+  public function name() {
+    if ($this->first_name && $this->last_name) {
+      $name = implode( ' ', [$this->first_name, $this->last_name] );
+    } elseif ( $this->first_name ) {
+      $name = $this->first_name;
+    } else {
+      $name = $this->display_name;
+    }
+    
+    return $name;
   }
 
   

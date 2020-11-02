2d8eed5cacbc3e97fbf4109943930593566e3537
Martin Cao <martin.g.cao@gmail.com>
2020-11-02 20:43:08 +1100

    Add functions from_id from_affid and get_sponsor

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -13,6 +13,26 @@ class User extends \WP_User {
   }
 
 
+  public static function from_id( int $wpid ) {
+    return \get_user_by( 'id', $wpid );
+  }
+
+
+  public static function from_affid( int $affid ) {
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'uap_affiliates';
+    $query = "SELECT uid FROM $table WHERE id = $affid";
+    $wpid = $wpdb->get_var( $query );
+
+    if ( empty( $wpid ) ) {
+      return false;
+    }
+
+    return self::from_id( $wpid );
+  }
+
+
   public function affid() {
     global $wpdb;
 
@@ -25,4 +45,20 @@ class User extends \WP_User {
     return $affid;
   }
 
+
+  public function get_sponsor() {
+    global $wpdb;
+
+    $table = $wpdb->prefix . 'uap_affiliate_referral_users_relations';
+    $query = "SELECT affiliate_id FROM $table WHERE referral_wp_uid = $this->id";
+    $sponsor_affid = $wpdb->get_var( $query );
+
+    if ( empty( $sponsor_affid ) ) {
+      return false;
+    }
+
+    return self::from_affid( $sponsor_affid );
+  }
+
+  
 }

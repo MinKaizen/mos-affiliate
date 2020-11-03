04557f02171e5bfb04f6beca463949879625053b
Martin Cao <martin.g.cao@gmail.com>
2020-11-03 14:59:11 +1100

    Add shortcode mos_level

---
+++ includes/classes/Shortcode/MosLevel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MOS\Affiliate\Shortcode;
+
+use MOS\Affiliate\Shortcode;
+use MOS\Affiliate\User;
+
+class MosLevel extends Shortcode {
+
+  protected $slug = 'mos_level';
+
+  public function shortcode_action( $args ): string {
+    $level = User::current()->level();
+    return $level;
+  }
+
+}

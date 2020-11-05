5823a134502cbc0b7d94676c0674d4b85d42b0d8
Martin Cao <martin.g.cao@gmail.com>
2020-11-05 14:17:15 +1100

    Add abilities config

---
+++ includes/config/abilities.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MOS\Affiliate;
+
+define( 'ABILITY_CAP_PREFIX', 'mos_access_' );
+
+define( 'ABILITIES', [
+  'access_monthly_partner',
+  'access_lifetime_partner',
+  'access_legacy_partner',
+  'mis',
+  'banner_mis',
+  'access_oto1',
+  'access_oto2',
+  'access_oto3',
+]);

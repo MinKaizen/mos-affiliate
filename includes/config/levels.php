7d57f5d0dec85bcfcb5a6b244358f3bf97a8c596
Martin Cao <martin.g.cao@gmail.com>
2020-11-05 14:17:40 +1100

    Add levels config

---
+++ includes/config/levels.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MOS\Affiliate;
+
+define( 'LEVELS', [
+  'free' => [
+    'name' => 'Free Member',
+    'slug' => 'free',
+    'order' => 1,
+    'abilities' => [
+      'access_free',
+    ],
+  ],
+  'monthly_partner' => [
+    'name' => 'Monthly Partner',
+    'slug' => 'monthly_partner',
+    'order' => 2,
+    'abilities' => [
+      'access_monthly_partner',
+      'mis',
+    ],
+  ],
+  'lifetime_partner' => [
+    'name' => 'Lifetime Partner',
+    'slug' => 'lifetime_partner',
+    'order' => 3,
+    'abilities' => [
+      'access_lifetime_partner',
+    ],
+  ],
+  'legacy_partner' => [
+    'name' => 'Legacy Partner',
+    'slug' => 'legacy_partner',
+    'order' => 4,
+    'abilities' => [
+      'access_legacy_partner',
+    ],
+  ],
+  'administrator' => [
+    'name' => 'Administrator',
+    'slug' => 'administrator',
+    'order' => 99,
+    'abilities' => [],
+  ],
+]);

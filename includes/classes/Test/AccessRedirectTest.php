14090e5c5a3f8232b18e91b7cb67cfec76c6d704
Martin Cao <martin.g.cao@gmail.com>
2020-11-17 08:52:45 +1100

    Flesh out AccessRedirectTest

--- includes/classes/Test/AccessRedirectTest.php
+++ includes/classes/Test/AccessRedirectTest.php
@@ -6,8 +6,58 @@ use MOS\Affiliate\Test;
 
 class AccessRedirectTest extends Test {
 
+  private $urls = [
+    [
+      'start' => '/monthly-partners-only',
+      'end' => '/no-access-monthly-partner',
+    ],
+    [
+      'start' => '/yearly-partners-only',
+      'end' => '/no-access-yearly-partner',
+    ],
+    [
+      'start' => '/free-members-only',
+      'end' => '/',
+    ],
+  ];
+
+
   public function main(): void {
-    $this->assert_true( true==false, 'true is false' );
+    foreach ( $this->urls as $pair ) {
+      $this->check_redirect( $pair['start'], $pair['end'] );
+    }
+  }
+
+
+  private function check_redirect( string $start, string $end ) {
+    $url = \home_url( $start );
+    $redirected = $this->get_redirect( $url );
+    $this->assert_equal( \home_url( $end ), $redirected, '' );
+  }
+
+
+  private function get_redirect( string $url ): string {
+    // Initialize a CURL session. 
+    $ch = curl_init(); 
+      
+    // Grab URL and pass it to the variable. 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+      
+    // Catch output (do NOT print!) 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+      
+    // Return follow location true 
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); 
+    $html = curl_exec($ch); 
+      
+    // Getinfo or redirected URL from effective URL 
+    $redirectedUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+      
+    // Close handle 
+    curl_close($ch); 
+    // echo "Original URL:   " . $url . "<br/>"; 
+    // echo "Redirected URL: " . $redirectedUrl . "<br/>"; 
+    return $redirectedUrl;
   }
 
 }

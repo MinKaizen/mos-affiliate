23ffbc8e75d0e39e5739df3b5b9b07ea1e98939e
Martin Cao <martin.g.cao@gmail.com>
2020-11-17 11:09:46 +1100

    Add a few test to user test

--- includes/classes/Test/UserTest.php
+++ includes/classes/Test/UserTest.php
@@ -3,11 +3,31 @@
 namespace MOS\Affiliate\Test;
 
 use MOS\Affiliate\Test;
+use MOS\Affiliate\User;
 
 class UserTest extends Test {
 
-  public function main(): void {
-    $this->assert_true( true==false );
+  private $username = "_wp_cli_test_user";
+  private $wpid;
+
+  public function _before(): void {
+
   }
+
+  public function main(): void {
+    $user = new User();
+
+    // doesn't exist
+    $this->assert_true( ! $user->exists(), "User doesn't exist" );
+
+    // constructor
+    $this->assert_instanceof( $user, 'MOS\Affiliate\User' );
+
+    // get_wpid()
+    $user->id = 42;
+    $this->assert_equal( $user->get_wpid(), 42, "get_wpid()", "expected" );
+
+  }
+
 
 }

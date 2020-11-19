b3864a161b3255efb432b209b28900b0e14ab42e
Martin Cao <martin.g.cao@gmail.com>
2020-11-19 19:17:28 +1100

    FIX: change equal to notequal in user_exists check

--- includes/classes/Test/UserShortcodeTest.php
+++ includes/classes/Test/UserShortcodeTest.php
@@ -42,7 +42,7 @@ class UserShortcodeTest extends Test {
   private function delete_user( int $id ): void {
     // Delete User
     \wp_delete_user( $id );
-    $user_exists = (\get_user_by( 'id', $id ) === false);
+    $user_exists = (\get_user_by( 'id', $id ) !== false);
     
     // Remove affiliate ID
     global $wpdb;

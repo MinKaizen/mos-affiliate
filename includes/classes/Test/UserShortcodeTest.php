9b9ac1693ad3a7474f300d71bec0ef22848aa326
Martin Cao <martin.g.cao@gmail.com>
2020-11-19 19:04:36 +1100

    Add function create_user()

--- includes/classes/Test/UserShortcodeTest.php
+++ includes/classes/Test/UserShortcodeTest.php
@@ -19,4 +19,24 @@ class UserShortcodeTest extends Test {
   }
 
   
+  private function create_user( string $username ): User {
+    // Create User
+    $id = \wp_insert_user([
+      'user_login' => $username,
+    ]);
+    $this->assert_is_int( $id, $id );
+    $this->db_notice( "$id - user created" );
+
+    // Register user as affiliate
+    $db = new Database();
+    $success = $db->register_affiliate( $id );
+    $this->assert_true_strict( $success );
+    $this->db_notice( "$id - registered as affiliate" );
+
+    $user = User::from_id( $id );
+
+    return $user;
+  }
+
+
 }

e71689ebf28ec928cbbfd2a7d4fda2cb54625f52
Martin Cao <martin.g.cao@gmail.com>
2020-11-20 15:28:37 +1100

    Create/delete post on construct/destruct

--- includes/classes/Test/AccessRedirectTest.php
+++ includes/classes/Test/AccessRedirectTest.php
@@ -5,17 +5,28 @@ namespace MOS\Affiliate\Test;
 use MOS\Affiliate\Test;
 use MOS\Affiliate\User;
 use \WP_CLI;
+use \WP_Post;
 
 use function \wp_insert_user;
 use function \wp_delete_user;
 use function \get_user_by;
 use function \remove_filter;
 use function \add_filter;
+use function \wp_insert_post;
+use function \wp_delete_post;
+use function \get_post;
+use function \wp_set_post_tags;
 
 class AccessRedirectTest extends Test {
 
   private $user;
   private $username = '3TQSX6qfj22oX7tgB5zIpV3RPZePfDAA';
+  private $post;
+  private $post_data = [
+    // 'post_author' => 1,
+    'post_title' => 'spyNukLYZxQ521u8ngnBoSKxrwwoBwSE',
+    'post_status' => 'public',
+  ];
   private $urls = [
     [
       'start' => '/monthly-partners-only',
@@ -41,12 +52,14 @@ class AccessRedirectTest extends Test {
 
     $this->user = $this->create_user( $this->username );
     $this->set_user();
+    $this->post = $this->create_post( $this->post_data );
   }
 
 
   public function __destruct() {
     $this->delete_user( $this->user->ID );
     $this->unset_user();
+    $this->delete_post( $this->post->ID );
   }
 
 
@@ -64,14 +77,14 @@ class AccessRedirectTest extends Test {
 
   private function set_user(): void {
     add_filter( 'mos_current_user', [$this, 'get_user'] );
-    WP_CLI::line('Filter added: set_user');
+    $this->db_notice( "filter added: {$this->user->ID}" );
   }
 
 
   private function unset_user(): void {
     $remove_success = remove_filter( 'mos_current_user', [$this, 'get_user'] );
     if ($remove_success) {
-      WP_CLI::line('Filter removed: set_user');
+      $this->db_notice("filter removed: {$this->user->ID}");
     }
   }
 
@@ -111,7 +124,7 @@ class AccessRedirectTest extends Test {
   private function create_user( string $username ): User {
     $id = wp_insert_user(['user_login' => $username]);
     $this->assert_is_int( $id, $id );
-    $this->db_notice( "$id - user created" );
+    $this->db_notice( "user created: $id" );
     $user = User::from_id( $id );
     return $user;
   }
@@ -121,7 +134,25 @@ class AccessRedirectTest extends Test {
     wp_delete_user( $id );
     $user_exists = (get_user_by( 'id', $id ) !== false);
     $this->assert_false_strict( $user_exists );
-    $this->db_notice( "$id - user deleted" );
+    $this->db_notice( "user deleted: $id" );
+  }
+
+
+  private function create_post( array $post_data ): WP_Post {
+    $post_id = wp_insert_post( $post_data, false );
+    $this->assert_not_equal_strict( $post_id, 0 );
+    $post = get_post( $post_id, 'OBJECT' );
+    $this->assert_instanceof( $post, 'WP_Post' );
+    $this->db_notice( "post created: $post_id" );
+    return $post;
+  }
+
+
+  private function delete_post( int $post_id ): void {
+    wp_delete_post( $post_id, true );
+    $post = get_post( $post_id );
+    $this->assert_true( empty( $post ), $post );
+    $this->db_notice( "post deleted: $post_id" );
   }
 
 }

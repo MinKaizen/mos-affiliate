b86b431a5b467fc0bd4ebd03c357eaa7ef48337a
Martin Cao <martin.g.cao@gmail.com>
2020-11-23 07:31:56 +1100

    Add http agent for curl

--- includes/classes/Test/AccessRedirectTest.php
+++ includes/classes/Test/AccessRedirectTest.php
@@ -27,6 +27,7 @@ class AccessRedirectTest extends Test {
   private $post;
   private $permalink;
   private $cookie_file;
+  private $http_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6';
 
 
   public function __construct() {

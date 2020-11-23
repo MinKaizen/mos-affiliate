bf2b068590313262d802a9fa3e2e4d20b37c664a
Martin Cao <martin.g.cao@gmail.com>
2020-11-23 12:29:56 +1100

    Rename CbBannersMis to BannersMis

--- includes/classes/Mis/BannersMis.php
+++ includes/classes/Mis/BannersMis.php
@@ -4,11 +4,11 @@ namespace MOS\Affiliate\Mis;
 
 use \MOS\Affiliate\Mis;
 
-class CbBannersMis extends Mis {
+class BannersMis extends Mis {
 
   protected $meta_key = parent::MIS_META_KEY_PREFIX . 'cb';
   protected $name = 'ClickBank (Banners)';
-  protected $slug = 'banner';
+  protected $slug = 'banners';
   protected $default = 'htlcb';
   protected $link_template = 'https://www.google.com/search?q=' . parent::MIS_LINK_PLACEHOLDER;
   protected $cap = \MOS\Affiliate\CAP_BANNER_MIS;

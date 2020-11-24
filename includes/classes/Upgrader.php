f2aa179e468983688e75ae8f1f964a5f5e7c5f50
Martin Cao <martin.g.cao@gmail.com>
2020-11-24 11:11:19 +1100

    Refactor Upgrader to use Migration class

--- includes/classes/Upgrader.php
+++ includes/classes/Upgrader.php
@@ -4,31 +4,25 @@ namespace MOS\Affiliate;
 
 class Upgrader {
 
+  const MIGRATIONS = [
+    'mis_migration',
+  ];
+
+  
   public static function upgrade() {
-    global $wpdb;
-
-    $sql = [];
-    $charset_collate = $wpdb->get_charset_collate();
-    $plugin_table_prefix = $wpdb->prefix . 'mos_';
-    
-    // mis table
-    $table_name = $plugin_table_prefix . 'mis';
-    $sql[] = "CREATE TABLE $table_name (
-      id int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      slug varchar(20) NOT NULL,
-      name varchar(63) NOT NULL,
-      default_value varchar(63) NOT NULL,
-      url varchar(255) NOT NULL,
-      KEY slug (slug)
-    ) $charset_collate;";
-    
-    // // another table
-    // $table_name = $plugin_table_prefix . 'table_name';
-    // $sql[] = ...
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    
-    \dbDelta( $sql );
+    self::run_migrations();
   }
+
+
+  private static function run_migrations(): void {
+    foreach ( self::MIGRATIONS as $migration_name ) {
+      $class_name = class_name( $migration_name, 'Migration' );
+      if ( class_exists( $class_name ) ) {
+        $migration = new $class_name();
+        $migration->run();
+      }
+    }
+  }
+
 
 }

cfa4444c3b21cefe9e5d03bdde8e2ffc40f3aca5
Martin Cao <martin.g.cao@gmail.com>
2020-11-25 15:11:24 +1100

    Add assertion for checking the columns paramter

--- includes/classes/Test/DatabaseClassTest.php
+++ includes/classes/Test/DatabaseClassTest.php
@@ -11,7 +11,9 @@ class DatabaseClassTest extends Test {
   public function test_get_row() {
     $db = new Database();
     $result = $db->get_row( 'users', ['ID=1'] );
+    $result = $db->get_row( 'users', ['ID=1'], ['ID'] );
     $this->assert_equal( $result['ID'], 1, $result );
+    $this->assert_false( $result['user_login'] );
   }
 
 

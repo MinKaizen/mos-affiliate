2aab15b724649c253acdd1cd742455ff31f734d8
Martin Cao <martin.g.cao@gmail.com>
2020-11-26 10:22:12 +1100

    Remove html table related stuff

--- includes/classes/CliCommand/TestCliCommand.php
+++ includes/classes/CliCommand/TestCliCommand.php
@@ -12,7 +12,6 @@ class TestCliCommand extends CliCommand {
   protected $command = 'test';
 
   private $tests = [
-    'html_table_class',
     'database_class',
     'pre_conditions',
     'migrations',

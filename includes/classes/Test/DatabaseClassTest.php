8ce3c9cfdea63e0deb506b5acaed13e86927e90b
Martin Cao <martin.g.cao@gmail.com>
2020-11-25 15:42:46 +1100

    Add assertion that result contains exactly 1 element (only 1 column was filtered

--- includes/classes/Test/DatabaseClassTest.php
+++ includes/classes/Test/DatabaseClassTest.php
@@ -15,6 +15,7 @@ class DatabaseClassTest extends Test {
     $result = $db->get_row( 'users', ['ID=1'] );
     $result = $db->get_row( 'users', ['ID=1'], ['ID'] );
     $this->assert_is_array( $result );
+    $this->assert_equal( count($result), 1, 'Result should only contain 1 column because of the filter' );
     $this->assert_equal( $result['ID'], 1, $result );
     $this->assert_false( $result['user_login'] );
   }

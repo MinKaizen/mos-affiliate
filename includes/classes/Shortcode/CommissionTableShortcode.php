cf47596c93c29c7882be0127ea0c650655864329
Martin Cao <martin.g.cao@gmail.com>
2020-11-26 10:21:08 +1100

    Use the view+controller in CommissionTableShortcode

--- includes/classes/Shortcode/CommissionTableShortcode.php
+++ includes/classes/Shortcode/CommissionTableShortcode.php
@@ -12,28 +12,7 @@ class CommissionTableShortcode extends AbstractShortcode {
   protected $slug = 'mos_commission_table';
 
   public function shortcode_action( $args ): string {
-    $table_data = [
-      [
-        'name' => 'Martin',
-        'email' => 'martin@gmail.com',
-        'username' => 'martin145',
-        'something_snake_case' => 'martin145',
-      ],
-      [
-        'name' => 'Tom',
-        'email' => 'tom@gmail.com',
-        'username' => 'tomhanks',
-        'something_snake_case' => 'tomhanks',
-      ],
-      [
-        'name' => 'Hayasa',
-        'email' => 'hayasa@gmail.com',
-        'username' => 'hayasaka<3',
-        'something_snake_case' => 'hayasaka<3',
-      ],
-    ];
-    $table = new HtmlTable( $table_data, ['id'=>'my_id', 'class'=>'my-class'] );
-    return get_view('html_table', ['table' => $table]);
+    return get_view( 'commission_table' );
   }
 
 }

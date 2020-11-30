33de1ec84327cae9a7495f13345b42a4f6f9b22a
Martin Cao <martin.g.cao@gmail.com>
2020-12-01 08:48:42 +1100

    Implement commission table test

--- includes/classes/Test/CommissionTableTest.php
+++ includes/classes/Test/CommissionTableTest.php
@@ -3,11 +3,145 @@
 namespace MOS\Affiliate\Test;
 
 use MOS\Affiliate\Test;
+use MOS\Affiliate\Controller;
+
+use function \MOS\Affiliate\ranstr;
 
 class CommissionTableTest extends Test {
 
-  public function test_test(): void {
-    // It works!
+
+  public function __construct() {
+    $this->_injected_user = $this->create_test_user();
+
+    $actor_data = [
+      'first_name' => 'Jill',
+      'last_name' => 'Smith',
+      'user_email' => ranstr() . "@" . ranstr() . ".com",
+    ];
+
+    $this->actor = $this->create_test_user( $actor_data );
+
+    $commission_data = [
+      [
+        'date' => '2020-11-05',
+        'amount' => 42,
+        'description' => 'Description 1',
+        'transaction_id' => 'asdf1',
+        'campaign' => 'facebook',
+        'actor_id' => $this->actor->ID,
+        'earner_id' => $this->_injected_user->ID,
+        'payout_date' => '2020-04-04',
+        'payout_method' => 'Bitcoin',
+        'payout_address' => 'sd54f5s4df6sd5f16sd54fs5d4f',
+        'payout_transaction_id' => '54654-5464',
+      ],
+      [
+        'date' => '2020-11-06',
+        'amount' => 56,
+        'description' => 'Description 2',
+        'transaction_id' => 'asdf2',
+        'campaign' => 'facebook2',
+        'actor_id' => $this->actor->ID,
+        'earner_id' => $this->_injected_user->ID,
+        'payout_date' => '2018-04-01',
+        'payout_method' => 'Bitcoin',
+        'payout_address' => 'sd54f5s4asdaf16sd54fs5d4f',
+        'payout_transaction_id' => '53354-5464',
+      ],
+      [
+        'date' => '2020-11-07',
+        'amount' => 42,
+        'description' => 'Description 3',
+        'transaction_id' => 'asdf3',
+        'campaign' => 'facebook',
+        'actor_id' => $this->actor->ID,
+        'earner_id' => $this->_injected_user->ID,
+        'payout_date' => '2020-04-04',
+        'payout_method' => 'Bitcoin',
+        'payout_address' => 'sd54f5s4df6sd5f36sd54fs5d4f',
+        'payout_transaction_id' => '54654-5464',
+      ],
+      [
+        'date' => '2021-11-08',
+        'amount' => 422,
+        'description' => 'Description 4',
+        'transaction_id' => 'asdf4',
+        'campaign' => 'facebook4',
+        'actor_id' => $this->actor->ID,
+        'earner_id' => $this->_injected_user->ID,
+      ],
+      [
+        'date' => '2001-11-08',
+        'amount' => 12,
+        'description' => 'Description 5',
+        'transaction_id' => 'asdf5',
+        'campaign' => 'facebook5',
+        'actor_id' => $this->actor->ID,
+        'earner_id' => $this->_injected_user->ID,
+        'payout_date' => '2010-04-04',
+        'payout_method' => 'Bitcoin',
+        'payout_address' => 'sd54f55465f16sd54fs5d4f',
+        'payout_transaction_id' => '54-5464',
+        'refund_date' => '2020-12-24',
+      ],
+    ];
+
+    foreach ( $commission_data as $commission ) {
+      $this->create_test_commission( $commission );
+    }
   }
+
+
+  public function test_rows(): void {
+    $controller = Controller::get_controller( 'commission_table' );;
+    $vars = $controller->get_vars();
+    $expected_rows = [
+      [
+        'date' => '2020-11-05',
+        'amount' => '$42.00',
+        'name' => 'Jill Smith',
+        'email' => $this->actor->user_email,
+        'product' => 'Description 1',
+        'campaign' => 'facebook',
+        'payment' => 'Bitcoin',
+      ],
+      [
+        'date' => '2020-11-06',
+        'amount' => '$56.00',
+        'name' => 'Jill Smith',
+        'email' => $this->actor->user_email,
+        'product' => 'Description 2',
+        'campaign' => 'facebook2',
+        'payment' => 'Bitcoin',
+      ],
+      [
+        'date' => '2020-11-07',
+        'amount' => '$42.00',
+        'name' => 'Jill Smith',
+        'email' => $this->actor->user_email,
+        'product' => 'Description 3',
+        'campaign' => 'facebook',
+        'payment' => 'Bitcoin',
+      ],
+    ];
+
+    function sort_by_date( array $commission1, array $commission2 ): int {
+      return $commission1['date'] <=> $commission2['date'];
+    }
+
+    foreach ( $expected_rows as $commission ) {
+      $this->assert_has_key( $commission, 'date' );
+    }
+    
+    foreach ( $vars['rows'] as $commission ) {
+      $this->assert_has_key( $commission, 'date' );
+    }
+
+    $expected_rows = usort( $expected_rows, __NAMESPACE__ . '\\sort_by_date' );
+    $vars['rows'] = usort( $vars['rows'], __NAMESPACE__ . '\\sort_by_date' );
+
+    $this->assert_equal( $expected_rows, $vars['rows'] );
+  }
+
 
 }

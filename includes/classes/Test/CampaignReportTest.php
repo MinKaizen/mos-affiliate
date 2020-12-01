fd7c9cbe5d3d1d00f906a7b4c64a15ca52fded62
Martin Cao <martin.g.cao@gmail.com>
2020-12-01 12:04:19 +1100

    Add test for Campaign Report

---
+++ includes/classes/Test/CampaignReportTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace MOS\Affiliate\Test;
+
+use MOS\Affiliate\Test;
+
+class CampaignReportTest extends Test {
+
+  public function test_test(): void {
+    // It works!
+  }
+
+}

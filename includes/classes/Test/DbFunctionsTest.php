209988beaa77f1d055f72dd35e7af663ec584091
Martin Cao <martin.g.cao@gmail.com>
2020-12-07 10:32:25 +1100

    Add pre test for user create/delete

--- includes/classes/Test/DbFunctionsTest.php
+++ includes/classes/Test/DbFunctionsTest.php
@@ -10,4 +10,18 @@ class DbFunctionsTest extends Test {
     echo "This is the DB Functions Test" . PHP_EOL;
   }
 
+
+  public function test_create_user(): void {
+    $user = $this->create_test_user();
+    $user = new \WP_User($user);
+    $user_from_db = get_user_by( 'id', $user->ID );
+    $this->assert_equal( $user, $user_from_db, "Generated user should equal user in db" );
+    
+    // Delete user
+    $this->delete_test_users();
+    $user_from_db = get_user_by( 'id', $user->ID );
+    $this->assert_false_strict( $user_from_db, "get_user_by() should return false after we delete users" );
+  }
+
+
 }

69a22d86e26b30b3e7454ee292e15c51f3d0288c
Martin Cao <martin.g.cao@gmail.com>
2020-12-08 13:50:57 +1100

    Add tests for logged out user

--- includes/classes/Test/CampaignReportTest.php
+++ includes/classes/Test/CampaignReportTest.php
@@ -3,11 +3,26 @@
 namespace MOS\Affiliate\Test;
 
 use MOS\Affiliate\Test;
+use MOS\Affiliate\Controller\CampaignReport;
 
 class CampaignReportTest extends Test {
 
-  public function test_test(): void {
-    
+  private $vars = [];
+
+
+  protected function _before(): void {
+    $controller = new CampaignReport();
+    $this->vars = $controller->get_vars();
+  }
+
+
+  public function test_get_campaigns_for_logged_out_user(): void {
+    $this->assert_equal( [], $this->vars['campaigns'] );
+  }
+
+
+  public function test_get_headers_for_logged_out_user(): void {
+    $this->assert_equal( [], $this->vars['headers'] );
   }
 
 }

c7b2bc7f5efca19ab36f4b16941bfa8f71c83a57
Martin Cao <martin.g.cao@gmail.com>
2020-12-10 10:12:44 +1100

    Add utils test for snake_to_pascal_case()

--- includes/classes/Test/UtilsTest.php
+++ includes/classes/Test/UtilsTest.php
@@ -5,6 +5,7 @@ namespace MOS\Affiliate\Test;
 use \MOS\Affiliate\Test;
 
 use function \MOS\Affiliate\class_name;
+use function \MOS\Affiliate\snake_to_pascal_case;
 
 class UtilsTest extends Test {
 
@@ -13,6 +14,13 @@ class UtilsTest extends Test {
     $this->assert_equal( class_name( 'hello', 'Folder' ), 'MOS\\Affiliate\\Folder\\Hello' );
     $this->assert_equal( class_name( 'hello_world', 'Folder' ), 'MOS\\Affiliate\\Folder\\HelloWorld' );
     $this->assert_equal( class_name( 'hello_world', 'Folder/subfold' ), 'MOS\\Affiliate\\Folder\\subfold\\HelloWorld' );
+  }
+
+
+  public function test_snake_to_pascal_case(): void {
+    $this->assert_equal( snake_to_pascal_case( 'hello' ), 'Hello' );
+    $this->assert_equal( snake_to_pascal_case( 'hello_world' ), 'HelloWorld' );
+    $this->assert_equal( snake_to_pascal_case( '_hello_world_' ), 'HelloWorld' );
   }
 
 }

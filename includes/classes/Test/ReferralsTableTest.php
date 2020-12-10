21bbe375b1108a2a8fb2add7588ba164aa3aee4a
Martin Cao <martin.g.cao@gmail.com>
2020-12-10 12:00:41 +1100

    Db create user and referrals

--- includes/classes/Test/ReferralsTableTest.php
+++ includes/classes/Test/ReferralsTableTest.php
@@ -45,7 +45,23 @@ class ReferralsTableTest extends Test {
       $this->users[$index]['username'] = ranstr(32);
       $this->users[$index]['email'] = ranstr(20) . '@' . ranstr(5) . '.' . ranstr(5);
     }
+
+    $this->_injected_user = $this->create_test_user();
+    $this->set_user();
+
+    foreach ( $this->users as $user ) {
+      $user_data = [
+        'user_registered' => $user['date'],
+        'user_login' => $user['username'],
+        'user_email' => $user['email'],
+        'display_name' => $user['name'],
+      ];
+
+      $db_user = $this->create_test_user( $user_data );
+      $db_user->set_role( $user['level'] );
+      $this->create_test_referral( $db_user->ID, $this->_injected_user->ID, $user['campaign'] );
     }
+    
   }
 
   public function test_controller(): void {

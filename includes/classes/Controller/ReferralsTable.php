d1b4cd8562cf23fb3d8184accd1fced24089dc83
Martin Cao <martin.g.cao@gmail.com>
2020-12-10 15:12:48 +1100

    Implement controller

--- includes/classes/Controller/ReferralsTable.php
+++ includes/classes/Controller/ReferralsTable.php
@@ -3,28 +3,52 @@
 namespace MOS\Affiliate\Controller;
 
 use MOS\Affiliate\Controller;
+use MOS\Affiliate\User;
+
+use function MOS\Affiliate\first_non_empty_element;
+use function MOS\Affiliate\is_dateable;
 
 class ReferralsTable extends Controller {
 
 
-  public function export_headers(): array {
-    return ['name', 'color'];
+  public function __construct() {
+    $this->referrals = $this->get_referrals();
   }
 
 
-  public function export_referrals(): array {
-    return [
-      [
-        'name' => 'apple',
-        'color' => 'red',
-      ],
-      [
-        'name' => 'banananaaanana',
-        'color' => 'yallow',
-      ],
-    ];
+  protected function export_referrals(): array {
+    return $this->referrals;
   }
 
+  
+  protected function export_headers(): array {
+    if ( empty( $this->referrals ) ) {
+      return [];
+    }
+    $first_element = first_non_empty_element( $this->referrals );
+    $headers = array_keys( $first_element );
+    return $headers;
+  }
+
+
+  private function get_referrals(): array {
+    $referral_objects = User::current()->get_referrals();
+    $referrals = [];
+
+    foreach ( $referral_objects as $user ) {
+      $referrals[] = [
+        'date' => $this->format_date( $user->user_registered ),
+        'username' => $user->get_username(),
+        'name' => $user->get_name(),
+        'email' => $user->get_email(),
+        'level' => $user->get_level(),
+        'progress' => $user->get_course_progress( 0 )['formatted'],
+        'campaign' => $user->get_campaign(),
+      ];
+    }
+
+    return $referrals;
+  }
 
 
   private function format_date( string $date ): string {

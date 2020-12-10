bb1dc7b2a6d8460e591993e0bd27b59e73414d9b
Martin Cao <martin.g.cao@gmail.com>
2020-12-10 15:22:53 +1100

    Apply datatables to referrals table

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 import CampaignReport from './components/CampaignReport';
+import ReferralsTable from './components/ReferralsTable';
 
 const components = [
   {
     class: CampaignReport,
     selector: '.js-campaign-report'
+  },
+  {
+    class: ReferralsTable,
+    selector: '.js-referrals-table'
   }
 ]
 

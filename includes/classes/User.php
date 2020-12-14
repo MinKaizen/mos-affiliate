66856b2f99796155b21b537805b41bb99d9db094
Martin Cao <martin.g.cao@gmail.com>
2020-12-15 07:36:32 +1100

    Remove function User->get_commissions()

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -241,22 +241,6 @@ class User extends \WP_User {
   }
 
 
-  public function get_commissions( bool $return_objects=true ): array {
-    global $wpdb;
-    $table = $wpdb->prefix . Migration\CommissionsMigration::TABLE_NAME;
-    $query = "SELECT * FROM $table WHERE earner_id = {$this->get_wpid()}";
-    $commissions = (array) $wpdb->get_results( $query, \ARRAY_A ) ;
-
-    if ( $return_objects ) {
-      foreach ( $commissions as &$commission ) {
-        $commission = Commission::create_from_array( $commission );
-      }
-    }
-
-    return $commissions;
-  }
-
-
   public function get_course_progress( int $course_id ): array {
     // Default values
     $course_progress = [

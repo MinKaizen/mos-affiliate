2365caf92b4156e4de9fc2204ab6bd602455e908
Martin Cao <martin.g.cao@gmail.com>
2020-12-17 11:55:56 +1100

    Remove unused functions in ClickbankEndpoint

--- includes/classes/Routes/ClickbankEndpoint.php
+++ includes/classes/Routes/ClickbankEndpoint.php
@@ -24,24 +24,4 @@ class ClickbankEndpoint extends REST_Route {
     return true;
   }
 
-  // Missing type hint. Could return array or object depending on json_decode()
-  private function adapter( WP_REST_Request $request ) {
-    $request_body = json_decode( $request->get_body() );
-    $data_encrypted = $request_body->notification;
-    $iv = $request_body->iv;
-    $data = json_decode( $this->decrypt_cb_notification( $data_encrypted, $iv, self::SECRET_KEY ) );
-    return $data;
-  }
-
-  private function decrypt_cb_notification( string $notification, string $iv, string $secret_key ): string {
-    $decrypted = trim(
-      openssl_decrypt(base64_decode($notification),
-      'AES-256-CBC',
-      substr(sha1($secret_key), 0, 32),
-      OPENSSL_RAW_DATA,
-      base64_decode($iv)), "\0..\32"
-    );
-    return $decrypted;
-  }
-
 }

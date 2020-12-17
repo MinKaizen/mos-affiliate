d2ee076592ca8c0863dc5447035bda57ff0cd84c
Martin Cao <martin.g.cao@gmail.com>
2020-12-17 11:40:06 +1100

    Apply adapter pattern to ClickbankEndpoint

--- includes/classes/Routes/ClickbankEndpoint.php
+++ includes/classes/Routes/ClickbankEndpoint.php
@@ -2,6 +2,7 @@
 
 namespace MOS\Affiliate\Routes;
 
+use \MOS\Affiliate\Adapters\ClickbankAdapter;
 use \MGC\REST_Route\REST_Route;
 use \WP_REST_Request;
 use \WP_REST_Response;
@@ -15,32 +16,12 @@ class ClickbankEndpoint extends REST_Route {
   protected $method = 'POST';
 
   public function handler( WP_REST_Request $request ): WP_REST_Response {
-    $data = $this->adapter( $request );
+    $data = new ClickbankAdapter( $request->get_body() );
     return $this->response( $data );
   }
 
   public function permission(): bool {
     return true;
-  }
-
-  // Missing type hint. Could return array or object depending on json_decode()
-  private function adapter( WP_REST_Request $request ) {
-    $request_body = json_decode( $request->get_body() );
-    $data_encrypted = $request_body->notification;
-    $iv = $request_body->iv;
-    $data = json_decode( $this->decrypt_cb_notification( $data_encrypted, $iv, self::SECRET_KEY ) );
-    return $data;
-  }
-
-  private function decrypt_cb_notification( string $notification, string $iv, string $secret_key ): string {
-    $decrypted = trim(
-      openssl_decrypt(base64_decode($notification),
-      'AES-256-CBC',
-      substr(sha1($secret_key), 0, 32),
-      OPENSSL_RAW_DATA,
-      base64_decode($iv)), "\0..\32"
-    );
-    return $decrypted;
   }
 
 }

b8e4ccdd3fcfca19b5bf5a490f49ab735f1e6a43
Martin Cao <martin.g.cao@gmail.com>
2020-12-18 12:27:06 +1100

    Check if msg is encrypted before decrypting

--- includes/classes/Routes/ClickbankEndpoint.php
+++ includes/classes/Routes/ClickbankEndpoint.php
@@ -16,12 +16,23 @@ class ClickbankEndpoint extends REST_Route {
   protected $method = 'POST';
 
   public function handler( WP_REST_Request $request ): WP_REST_Response {
-    $original_json = $this->extract_decrypted_msg( $request->get_body() );
+    $body = $request->get_body();
+    if ( $this->is_encrypted( $body ) ) {
+      $original_json = $this->extract_decrypted_msg( $body );
+    } else {
+      $original_json = $body;
+    }
     $original_object = json_decode( $original_json );
     $adapted_object = new ClickbankEventAdapter( $original_json );
     \do_action( 'clickbank_event_raw', $original_object );
     \do_action( 'clickbank_event', $adapted_object );
     return $this->response( $adapted_object );
+  }
+
+  private function is_encrypted( string $json ): bool {
+    $data = json_decode( $json );
+    $encrypted = ( isset( $data->iv ) && isset( $data->notification ) && is_string( $data->notification ) );
+    return $encrypted;
   }
 
   private function extract_decrypted_msg( string $json ): string {

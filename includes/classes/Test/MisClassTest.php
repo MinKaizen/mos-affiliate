2ac2e5cffbccebd2af77b77a2f40220f2aab5e02
Martin Cao <martin.g.cao@gmail.com>
2020-12-30 17:31:02 +1100

    Implement test_constructor()

--- includes/classes/Test/MisClassTest.php
+++ includes/classes/Test/MisClassTest.php
@@ -7,8 +7,25 @@ use \MOS\Affiliate\MIS;
 
 class MisClassTest extends Test {
 
-  public function test_temp(): void {
+  // NOTE: All tests relate to the mis.json config file
+
+  public function test_constructor(): void {
+    // Valid
+    $gr_mis = new MIS( 'gr' );
+    $this->assert_true( $gr_mis->exists, 'MIS for gr should exist' );
     
+    $cb_mis = new MIS( 'cb' );
+    $this->assert_true( $cb_mis->exists, 'MIS for cb should exist' );
+    
+    $cm_mis = new MIS( 'cm' );
+    $this->assert_true( $cm_mis->exists, 'MIS for cm should exist' );
+
+    // Invalid
+    $something_else_mis = new MIS( 'something_else' );
+    $this->assert_false( $something_else_mis->exists, 'MIS for something_else should not exist' );
+    
+    $blah_mis = new MIS( 'blah' );
+    $this->assert_false( $blah_mis->exists, 'MIS for blah should not exist' );
   }
 
 }

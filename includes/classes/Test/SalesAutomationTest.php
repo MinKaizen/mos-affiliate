d912f927cddb4ee3b9e2bbe8660aa75005577474
Martin Cao <martin.g.cao@gmail.com>
2021-01-02 10:16:02 +1100

    Rename variable data to sale

--- includes/classes/Test/SalesAutomationTest.php
+++ includes/classes/Test/SalesAutomationTest.php
@@ -20,25 +20,25 @@ class SalesAutomationTest extends Test {
   }
 
   public function test_update_commissions_on_sale(): void {
-    $data = new ClickbankEvent();
-    $data->date = "2021-01-01";
-    $data->amount = 197;
-    $data->product_id = 100;
-    $data->product_name = self::TEST_COMMISSION_DESCRIPTION;
-    $data->transaction_id = ranstr(32);
-    $data->transaction_type = "SALE";
-    $data->cb_affiliate = ranstr(6);
-    $data->campaign = ranstr(6);
-    $data->customer_wpid = $this->user->get_wpid();
-    $data->customer_username = $this->user->get_username();
-    $data->customer_name = $this->user->get_name();
-    $data->customer_email = $this->user->get_email();
-    $data->sponsor_wpid = $this->sponsor->get_wpid();
-    $data->sponsor_username = $this->sponsor->get_username();
-    $data->sponsor_name = $this->sponsor->get_name();
-    $data->sponsor_email = $this->sponsor->get_email();
+    $sale = new ClickbankEvent();
+    $sale->date = "2021-01-01";
+    $sale->amount = 197;
+    $sale->product_id = 100;
+    $sale->product_name = self::TEST_COMMISSION_DESCRIPTION;
+    $sale->transaction_id = ranstr(32);
+    $sale->transaction_type = "SALE";
+    $sale->cb_affiliate = ranstr(6);
+    $sale->campaign = ranstr(6);
+    $sale->customer_wpid = $this->user->get_wpid();
+    $sale->customer_username = $this->user->get_username();
+    $sale->customer_name = $this->user->get_name();
+    $sale->customer_email = $this->user->get_email();
+    $sale->sponsor_wpid = $this->sponsor->get_wpid();
+    $sale->sponsor_username = $this->sponsor->get_username();
+    $sale->sponsor_name = $this->sponsor->get_name();
+    $sale->sponsor_email = $this->sponsor->get_email();
 
-    \do_action( 'clickbank_event', $data );
+    \do_action( 'clickbank_event', $sale );
 
      // Call cron so that our async hook gets called
     \wp_remote_get( \home_url( '/wp-cron.php' ) );
@@ -49,26 +49,26 @@ class SalesAutomationTest extends Test {
     global $wpdb;
     $table = $wpdb->prefix . CommissionsMigration::TABLE_NAME;
     $conditions = [
-      "actor_id = $data->customer_wpid",
-      "earner_id = $data->sponsor_wpid",
-      "transaction_id = '$data->transaction_id'",
-      "campaign = '$data->campaign'",
+      "actor_id = $sale->customer_wpid",
+      "earner_id = $sale->sponsor_wpid",
+      "transaction_id = '$sale->transaction_id'",
+      "campaign = '$sale->campaign'",
     ];
     $query = "SELECT * FROM $table WHERE " . implode( ' AND ', $conditions );
     $commission = $wpdb->get_row( $query, 'OBJECT' );
 
     $this->assert_not_empty( $commission, $query );
-    $this->assert_equal( $commission->date, $data->date);
-    $this->assert_equal( $commission->amount, $data->amount);
-    $this->assert_equal( $commission->description, $data->product_name);
-    $this->assert_equal( $commission->transaction_id, $data->transaction_id);
-    $this->assert_equal( $commission->campaign, $data->campaign);
-    $this->assert_equal( $commission->actor_id, $data->customer_wpid);
-    $this->assert_equal( $commission->earner_id, $data->sponsor_wpid);
-    $this->assert_equal( $commission->payout_date, $data->date);
+    $this->assert_equal( $commission->date, $sale->date);
+    $this->assert_equal( $commission->amount, $sale->amount);
+    $this->assert_equal( $commission->description, $sale->product_name);
+    $this->assert_equal( $commission->transaction_id, $sale->transaction_id);
+    $this->assert_equal( $commission->campaign, $sale->campaign);
+    $this->assert_equal( $commission->actor_id, $sale->customer_wpid);
+    $this->assert_equal( $commission->earner_id, $sale->sponsor_wpid);
+    $this->assert_equal( $commission->payout_date, $sale->date);
     $this->assert_equal( $commission->payout_method, 'Clickbank');
-    $this->assert_equal( $commission->payout_address, $data->cb_affiliate);
-    $this->assert_equal( $commission->payout_transaction_id, $data->transaction_id);
+    $this->assert_equal( $commission->payout_address, $sale->cb_affiliate);
+    $this->assert_equal( $commission->payout_transaction_id, $sale->transaction_id);
   }
 
 }

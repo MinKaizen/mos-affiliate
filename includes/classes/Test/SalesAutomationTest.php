704716f1130e5f458f10b0983673df7217325076
Martin Cao <martin.g.cao@gmail.com>
2021-01-02 09:31:04 +1100

    Add sleep before asserting

--- includes/classes/Test/SalesAutomationTest.php
+++ includes/classes/Test/SalesAutomationTest.php
@@ -22,10 +22,10 @@ class SalesAutomationTest extends Test {
     $data->date = "2021-01-01";
     $data->amount = 197;
     $data->product_id = 100;
-    $data->product_name = "mos_affiliate_test";
+    $data->product_name = self::TEST_COMMISSION_DESCRIPTION;
     $data->transaction_id = ranstr(32);
     $data->transaction_type = "SALE";
-    $data->cb_affiliate = "mos_affiliate_test";
+    $data->cb_affiliate = ranstr(6);
     $data->campaign = ranstr(6);
     $data->customer_wpid = $this->user->get_wpid();
     $data->customer_username = $this->user->get_username();
@@ -37,8 +37,12 @@ class SalesAutomationTest extends Test {
     $data->sponsor_email = $this->sponsor->get_email();
 
     \do_action( 'clickbank_event', $data );
+
      // Call cron so that our async hook gets called
-     \wp_remote_get( \home_url( '/' ) );
+    \wp_remote_get( \home_url( '/wp-cron.php' ) );
+
+    // Give it some time to resolve
+    sleep(1);
 
     global $wpdb;
     $table = $wpdb->prefix . CommissionsMigration::TABLE_NAME;
@@ -51,11 +55,7 @@ class SalesAutomationTest extends Test {
     $query = "SELECT * FROM $table WHERE " . implode( ' AND ', $conditions );
     $commission = $wpdb->get_row( $query, 'OBJECT' );
 
-    $this->assert_not_empty( $commission );
-
-    // If the commission exists, delete it!
-    $wpdb->delete( $table, ['id' => $commission->id], ['id' => '%d'] );
-    $this->db_notice( 'commission deleted: ' . $commission->id );
+    $this->assert_not_empty( $commission, $query );
   }
 
 }

5375f79297be7c5588b96cf2f7daff5bdbc34f7a
Martin Cao <martin.g.cao@gmail.com>
2021-01-04 11:05:47 +1100

    Amend to reflect new config file

--- includes/classes/Test/ProductClassTest.php
+++ includes/classes/Test/ProductClassTest.php
@@ -9,8 +9,9 @@ class ProductClassTest extends Test {
 
   public function test_construct_from_slug(): void {
     $product = Product::from_slug( 'monthly_partner' );
-    $this->assert_equal( $product->cb_id, 1 );
+    $this->assert_equal( $product->cb_id, 1000 );
     $this->assert_equal( $product->name, "Monthly Partner" );
+    $this->assert_equal( $product->slug, "monthly_partner" );
     $this->assert_equal( $product->access_meta_key, "mos_access_monthly_partner" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-monthly-partner" );
     $this->assert_equal( $product->price, 7 );
@@ -19,50 +20,57 @@ class ProductClassTest extends Test {
     $this->assert_equal( $product->rebill_period, 31 );
     
     $product = Product::from_slug( 'fb_toolkit' );
-    $this->assert_equal( $product->cb_id, 2 );
+    $this->assert_equal( $product->cb_id, 51 );
     $this->assert_equal( $product->name, "Ultimate Facebook Toolkit" );
+    $this->assert_equal( $product->slug, "fb_toolkit" );
     $this->assert_equal( $product->access_meta_key, "mos_access_fb_toolkit" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-fb-toolkit" );
     $this->assert_equal( $product->price, 197 );
     
     $product = Product::from_slug( 'lead_system' );
-    $this->assert_equal( $product->cb_id, 3 );
+    $this->assert_equal( $product->cb_id, 52 );
     $this->assert_equal( $product->name, "Six Figure Lead Gen System" );
+    $this->assert_equal( $product->slug, "lead_system" );
     $this->assert_equal( $product->access_meta_key, "mos_access_lead_system" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-lead-system" );
     $this->assert_equal( $product->price, 197 );
     
     $product = Product::from_slug( 'authority_bonuses' );
-    $this->assert_equal( $product->cb_id, 4 );
+    $this->assert_equal( $product->cb_id, 53 );
     $this->assert_equal( $product->name, "Ultimate Authority Bonuses" );
+    $this->assert_equal( $product->slug, "authority_bonuses" );
     $this->assert_equal( $product->access_meta_key, "mos_access_authority_bonuses" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-authority-bonuses" );
     $this->assert_equal( $product->price, 197 );
     
     $product = Product::from_slug( 'yearly_partner' );
-    $this->assert_equal( $product->cb_id, 5 );
+    $this->assert_equal( $product->cb_id, 54 );
     $this->assert_equal( $product->name, "Yearly Partner" );
+    $this->assert_equal( $product->slug, "yearly_partner" );
     $this->assert_equal( $product->access_meta_key, "mos_access_yearly_partner" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-yearly-partner" );
     $this->assert_equal( $product->price, 497 );
     
     $product = Product::from_slug( 'lifetime_partner' );
     $this->assert_equal( $product->name, "Lifetime Partner" );
+    $this->assert_equal( $product->slug, "lifetime_partner" );
     $this->assert_equal( $product->access_meta_key, "mos_access_lifetime_partner" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-lifetime-partner" );
     $this->assert_equal( $product->price, 1997 );
     
     $product = Product::from_slug( 'coaching' );
     $this->assert_equal( $product->name, "Personal Coaching with Chuck" );
+    $this->assert_equal( $product->slug, "coaching" );
     $this->assert_equal( $product->access_meta_key, "mos_access_coaching" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-coaching" );
     $this->assert_equal( $product->price, 8997 );
   }
 
   public function test_construct_from_cb_id(): void {
-    $product = Product::from_cb_id( 1 );
-    $this->assert_equal( $product->cb_id, 1 );
+    $product = Product::from_cb_id( 1000 );
+    $this->assert_equal( $product->cb_id, 1000 );
     $this->assert_equal( $product->name, "Monthly Partner" );
+    $this->assert_equal( $product->slug, "monthly_partner" );
     $this->assert_equal( $product->access_meta_key, "mos_access_monthly_partner" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-monthly-partner" );
     $this->assert_equal( $product->price, 7 );
@@ -70,30 +78,34 @@ class ProductClassTest extends Test {
     $this->assert_equal( $product->rebill_price, 47 );
     $this->assert_equal( $product->rebill_period, 31 );
     
-    $product = Product::from_cb_id( 2 );
-    $this->assert_equal( $product->cb_id, 2 );
+    $product = Product::from_cb_id( 51 );
+    $this->assert_equal( $product->cb_id, 51 );
     $this->assert_equal( $product->name, "Ultimate Facebook Toolkit" );
+    $this->assert_equal( $product->slug, "fb_toolkit" );
     $this->assert_equal( $product->access_meta_key, "mos_access_fb_toolkit" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-fb-toolkit" );
     $this->assert_equal( $product->price, 197 );
     
-    $product = Product::from_cb_id( 3 );
-    $this->assert_equal( $product->cb_id, 3 );
+    $product = Product::from_cb_id( 52 );
+    $this->assert_equal( $product->cb_id, 52 );
     $this->assert_equal( $product->name, "Six Figure Lead Gen System" );
+    $this->assert_equal( $product->slug, "lead_system" );
     $this->assert_equal( $product->access_meta_key, "mos_access_lead_system" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-lead-system" );
     $this->assert_equal( $product->price, 197 );
     
-    $product = Product::from_cb_id( 4 );
-    $this->assert_equal( $product->cb_id, 4 );
+    $product = Product::from_cb_id( 53 );
+    $this->assert_equal( $product->cb_id, 53 );
     $this->assert_equal( $product->name, "Ultimate Authority Bonuses" );
+    $this->assert_equal( $product->slug, "authority_bonuses" );
     $this->assert_equal( $product->access_meta_key, "mos_access_authority_bonuses" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-authority-bonuses" );
     $this->assert_equal( $product->price, 197 );
     
-    $product = Product::from_cb_id( 5 );
-    $this->assert_equal( $product->cb_id, 5 );
+    $product = Product::from_cb_id( 54 );
+    $this->assert_equal( $product->cb_id, 54 );
     $this->assert_equal( $product->name, "Yearly Partner" );
+    $this->assert_equal( $product->slug, "yearly_partner" );
     $this->assert_equal( $product->access_meta_key, "mos_access_yearly_partner" );
     $this->assert_equal( $product->no_access_url_path, "/no-access-yearly-partner" );
     $this->assert_equal( $product->price, 497 );

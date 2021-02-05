736457f3253567fab1c11c71dd2f36f93f0410c8
Martin Cao <martin.g.cao@gmail.com>
2021-02-05 14:40:25 +1100

    Add FilterHook MosMisObject

---
+++ includes/classes/FilterHooks/MosMisObject.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace MOS\Affiliate\FilterHooks;
+
+use \MOS\Affiliate\FilterHook;
+use function \MOS\Affiliate\mis_object;
+
+class MosMisObject extends FilterHook {
+
+  protected $hook = 'mos_mis_object';
+  protected $args = 2;
+
+  public function handler( $fallback, $slug ): object {
+      return mis_object( $slug );
+  }
+
+}

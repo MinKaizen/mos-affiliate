ec259b5ecd9a1e52c20b191306f5a75235d9a26a
Martin Cao <martin.g.cao@gmail.com>
2021-02-11 13:04:12 +1100

    Add ACF UserAccessACF

--- includes/classes/Plugin.php
+++ includes/classes/Plugin.php
@@ -58,6 +58,7 @@ class Plugin {
     $this->register_classes_from_folder( 'Routes' );
     $this->register_classes_from_folder( 'ActionHooks' );
     $this->register_classes_from_folder( 'FilterHooks' );
+    $this->register_classes_from_folder( 'ACF' );
   }
 
 

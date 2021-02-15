7fa6fdbcfa24cd0238d7e51e9361b51aa41b52f6
Martin Cao <martin.g.cao@gmail.com>
2021-02-16 08:47:24 +1100

    Fix SponsorMisLinkShortcode not showing the default value

--- includes/classes/Shortcode/SponsorMisLinkShortcode.php
+++ includes/classes/Shortcode/SponsorMisLinkShortcode.php
@@ -4,6 +4,8 @@ namespace MOS\Affiliate\Shortcode;
 
 use MOS\Affiliate\AbstractShortcode;
 use MOS\Affiliate\User;
+use function MOS\Affiliate\mis_generate_link;
+use function MOS\Affiliate\mis_default_value;
 
 class SponsorMisLinkShortcode extends AbstractShortcode {
 
@@ -14,7 +16,18 @@ class SponsorMisLinkShortcode extends AbstractShortcode {
   ];
 
   public function shortcode_action( $args ): string {
-    $link = User::current()->sponsor()->get_mis_link( $args['network'] );
+    $sponsor = User::current()->sponsor();
+    $value = $sponsor->get_mis( $args['network'] );
+
+    if ( !$sponsor->exists() ) {
+      $value = mis_default_value( $args['network'] );
+    } elseif ( !$sponsor->qualifies_for_mis( $args['network'] ) ) {
+      $value = mis_default_value( $args['network'] );
+    } elseif ( empty( $value ) ) {
+      $value = mis_default_value( $args['network'] );
+    }
+
+    $link = mis_generate_link( $args['network'], $value );
     return $link;
   }
 

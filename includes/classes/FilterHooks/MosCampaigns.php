23d03d3d85d1abdd1513a19139b21e4257f68670
Martin Cao <martin.g.cao@gmail.com>
2021-02-18 10:30:05 +1100

    Remove addons from Campaigns report

--- includes/classes/FilterHooks/MosCampaigns.php
+++ includes/classes/FilterHooks/MosCampaigns.php
@@ -119,9 +119,6 @@ class MosCampaigns extends FilterHook {
       'yearly_partner',
       'lifetime_partner',
       'coaching',
-      'fb_toolkit',
-      'lead_system',
-      'authority_bonuses',
     ];
 
     foreach ( $campaigns as &$campaign ) {

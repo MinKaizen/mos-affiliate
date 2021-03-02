e450ce1afc028d10ad72ffc441df762e97b68baf
Martin Cao <martin.g.cao@gmail.com>
2021-03-02 14:59:36 +1100

    Refactor Filter MosUserLevelSlug to accept user_id

--- includes/classes/FilterHooks/MosUserLevelSlug.php
+++ includes/classes/FilterHooks/MosUserLevelSlug.php
@@ -4,13 +4,23 @@ namespace MOS\Affiliate\FilterHooks;
 
 use \MOS\Affiliate\FilterHook;
 use \MOS\Affiliate\User;
+use function \get_current_user_id;
 
 class MosUserLevelSlug extends FilterHook {
 
   protected $hook = 'mos_user_level_slug';
+  protected $args = 2;
 
-  public function handler(): string {
-    return User::current()->get_level_slug();
+  public function handler( $fallback, $id=0 ): string {
+    if ( !$id ) {
+      $id = get_current_user_id();
+    }
+
+    if ( !$id ) {
+      return User::current()->get_level_slug();
+    }
+
+    return User::from_id( $id )->get_level_slug();
   }
 
 }

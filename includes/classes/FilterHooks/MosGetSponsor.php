7385dfbca6a170f61d0797c5e51af7cb37c2459a
Martin Cao <martin.g.cao@gmail.com>
2021-03-02 16:53:48 +1100

    Add filter MosGetSponsor

---
+++ includes/classes/FilterHooks/MosGetSponsor.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace MOS\Affiliate\FilterHooks;
+
+use \MOS\Affiliate\FilterHook;
+use \MOS\Affiliate\User;
+use function \get_current_user_id;
+
+class MosGetSponsor extends FilterHook {
+
+  protected $hook = 'mos_get_sponsor';
+  protected $args = 2;
+
+  public function handler( $fallback, $id=0  ): User {
+    if ( !$id ) {
+      $id = get_current_user_id();
+    }
+
+    if ( !$id ) {
+      return User::current()->sponsor();
+    }
+
+    return User::from_id( $id )->sponsor();
+  }
+
+}

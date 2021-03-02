4ca4e3b3ca7224f82cae5ba8924617489abc88d9
Martin Cao <martin.g.cao@gmail.com>
2021-03-02 14:09:34 +1100

    Add action hook CbEvent_WiserNotify

---
+++ includes/classes/ActionHooks/CbEvent_WiserNotify.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace MOS\Affiliate\ActionHooks;
+
+use \MOS\Affiliate\ActionHook;
+use \MGC\Logger\Logger;
+use \MOS\Affiliate\DataStructs\ClickbankEvent;
+use function wp_remote_post;
+
+class CbEvent_WiserNotify extends ActionHook {
+
+  const TRANSACTION_TYPES = ['SALE', 'TEST_SALE'];
+  const WEBHOOK_URL = 'https://is.wisernotify.com/api/custom/log?ti=2h0helkgpqcn8w&fuid=603d76c7d1d139001333366d';
+
+  protected $hook = 'clickbank_event';
+
+  public function __construct() {
+    $this->logger = new Logger( 'mos-affiliate', 'clickbank_wiser_notify.log' );
+  }
+
+  public function handler( $data ): void {
+    if ( !( $data instanceof ClickbankEvent ) ) {
+      return;
+    }
+
+    $args = [
+      'headers' => [
+        'Accept' => 'application/json',
+        'Content-type' => 'application/json',
+      ],
+      'body' => $data->json(),
+    ];
+    wp_remote_post( self::WEBHOOK_URL, $args );
+    $this->logger->log( $args['body'] );
+  }
+
+}

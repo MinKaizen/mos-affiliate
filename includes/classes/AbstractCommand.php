1d06b30b3cae29aefb45f93c36332d5e81e652c6
Martin Cao <martin.g.cao@gmail.com>
2021-06-03 10:56:43 +1000

    Add function execute_async() to class AbstractCommand

--- includes/classes/AbstractCommand.php
+++ includes/classes/AbstractCommand.php
@@ -4,6 +4,12 @@ namespace MOS\Affiliate;
 
 abstract class AbstractCommand {
 
+  private $action_hook = 'execute_command';
+
   abstract function execute(): void;
+
+  public function execute_async(): void {
+    wp_schedule_single_event( time(), $this->action_hook, [$this] );
+  }
 
 }
